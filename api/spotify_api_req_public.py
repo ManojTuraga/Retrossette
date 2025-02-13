4fad8db1ff3aa8fba0d2fb678c4cf422d20dd2ba
Ceres Botkin <ceres.x.botkin@ku.edu>
2025-02-13 14:10:09 -0600

    Added Script for Public Spotify API Requests
    + Added script for public client spotify requests
    + Added function to get and managr server API token

---
+++ api/spotify_api_req_public.py
@@ -0,0 +1,37 @@
+'''
+Title: Public Spotify API Requests
+Author: Ceres Botkin
+Description: Basic functions to request PUBLIC spotify data. Uses client API as
+opposed to user API. Reccomended to access public information 
+'''
+# Package Requirements
+import time
+import requests
+import json
+
+from server_auth import request_spotify_client_api_token
+
+# Variables to store API token details
+client_api_token = None
+client_token_expire_time = 0
+
+# Checks to see if there is a valid client auth token. If not, request one
+def get_client_api_token():
+    global client_api_token, client_token_expire_time
+    # Get current time
+    curr_time = int(time.time())
+    
+    # Get request api token if none exists or if expired
+    if ((client_token_expire_time == None) or
+        (curr_time >= client_token_expire_time)):
+        # Request client api token
+        api_dict = request_spotify_client_api_token()
+
+        # Update global variables
+        client_api_token = api_dict['access_token']
+        client_token_expire_time = curr_time + api_dict['expires_in'] - 60
+
+    # Return API token
+    return client_api_token
+
+
