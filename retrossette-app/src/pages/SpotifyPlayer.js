845384e26c492d53440fc66992aea6a340169257
YAKU-Student <ceres.x.botkin@ku.edu>
2025-03-29 22:03:03 -0500

    Added Boombox Component

--- retrossette-app/src/pages/SpotifyPlayer.js
+++ retrossette-app/src/pages/SpotifyPlayer.js
@@ -9,6 +9,11 @@ OF OUR OWN WEBPLAYER, THEN WE WILL PROVIDE OUR OWN DOCUMENTATION
 
 import React, { useEffect, useState } from 'react';
 
+// Sleep for ms milliseconds
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const track = {
     name: "",
     album: {
@@ -19,6 +24,26 @@ const track = {
     artists: [
         { name: "" }
     ]
+}
+
+// Sound effect for pressing a button on the cassette player
+const button_sfx = new Audio('../boombox_assets/button_press_sfx.mp3')
+
+// Controlls the player when user pressed button button with player player
+async function pressCassetteButton(button, player) {
+    const buttonWait = 100; // Amount to wait before completing action
+
+    // Play button press sound effect
+    button_sfx.play();
+
+    // Determins what happens when button is pressed
+    if (button == 'playpause') {
+	sleep(buttonWait).then(() => { player.togglePlay(); });
+    } else if (button == 'rewind') {
+	sleep(buttonWait).then(() => { player.previousTrack(); });
+    } else if (button == 'fastforward') {
+	sleep(buttonWait).then(() => { player.nextTrack(); });
+    }
 }
 
 const SpotifyPlayer = ({ token, listOfSongs }) => {
@@ -129,36 +154,48 @@ const SpotifyPlayer = ({ token, listOfSongs }) => {
         );
     } else {
         return (
-            <>
-                <div className="container">
-                    <div className="main-wrapper">
+          <div className="container">
+              <div className="main-wrapper">
+		  
+                  <img src={require("../boombox_assets/player.png")}
+		       style={{position: 'absolute', top: '0px', left: '0px', zIndex: '2'}}
+		       alt="" />
+		  <img src={is_paused ? require("../boombox_assets/pp_pressed.png")
+			    : require("../boombox_assets/pp_unpressed.png") }
+		       style={{position: 'absolute', top: '0px', left: '0px', zIndex: '1'}}
+		       alt="" />
 
-                        <img src={current_track.album.images[0].url} className="now-playing__cover" alt="" />
+		  <img src={require("../boombox_assets/rr_unpressed.png") }
+		       style={{position: 'absolute', top: '0px', left: '0px', zIndex: '1'}}
+		       alt="" />
 
-                        <div className="now-playing__side">
-                            <div className="now-playing__name">{current_track.name}</div>
-                            <div className="now-playing__artist">{current_track.artists[0].name}</div>
+		  <img src={require("../boombox_assets/ff_unpressed.png") }
+		       style={{position: 'absolute', top: '0px', left: '0px', zIndex: '1'}}
+		       alt="" />
 
-                            <button className="btn-spotify" onClick={() => { player.previousTrack() }} >
-                                &lt;&lt;
-                            </button>
+		  <button style={{position: 'absolute', top: '58px', left: '476px', zIndex: '10',
+				  width: '84px', height: '56px'}}
+			  onClick={() => { pressCassetteButton('playpause', player); }}>
+		  </button>
 
-                            <button className="btn-spotify" onClick={() => { player.togglePlay() }} >
-                                {is_paused ? "PLAY" : "PAUSE"}
-                            </button>
+		  <button style={{position: 'absolute', top: '58px', left: '375px', zIndex: '10',
+				  width: '84px', height: '56px'}}
+			  onClick={() => { pressCassetteButton('rewind', player); }}>
+		  </button>
 
-                            <button className="btn-spotify" onClick={() => { player.nextTrack() }} >
-                                &gt;&gt;
-                            </button>
-                            
-                            <button className="btn-spotify" onClick={() => playTracks(listOfSongs)} >
-                                Play Tracks
-                            </button>
+		  <button style={{position: 'absolute', top: '58px', left: '577px', zIndex: '10',
+				  width: '84px', height: '56px'}}
+			  onClick={() => { pressCassetteButton('fastforward', player); }}>
+		  </button>
 
-                        </div>
-                    </div>
-                </div>
-            </>
+		  {/*
+                  <div className="now-playing__side">
+                      <div className="now-playing__name">{current_track.name}</div>
+                      <div className="now-playing__artist">{current_track.artists[0].name}</div>
+                  </div>
+		   */}
+	      </div>
+          </div>
         );
     }
 }
